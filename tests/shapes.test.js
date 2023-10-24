b6c0ece6625df275e78c7e5bb5aea169faa1e71c
kboyd <kboyd20020202@gmail.com>
2023-10-24 11:20:52 -0500

    added the set color method to the classes of each shape so that the tests can run

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -4,19 +4,19 @@ describe('shapes', () => {
 
 it('should make the Triangle blue', () => {
 const shape = new Triangle();
-shape.shapeColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+shape.setColor("blue");
+expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue"/>');
 });
 
 it('should make the Circle blue', () => {
 const shape1 = new Circle();
-shape1.shapeColor("blue");
-expect(shape1.render()).toEqual('<circle cx="150" cy="100" r="80" fill="blue" />');
+shape1.setColor("blue");
+expect(shape1.render()).toEqual('<circle cx="150" cy="100" r="80" fill="blue"/>');
 });
 
 it('should make the Square blue', () => {
 const shape2 = new Square();
-shape2.shapeColor("blue");
-expect(shape2.render()).toEqual('<rect width="100" height="100" fill="blue" />');
+shape2.setColor("blue");
+expect(shape2.render()).toEqual('<rect width="100" height="100" fill="blue"/>');
     });
 });
